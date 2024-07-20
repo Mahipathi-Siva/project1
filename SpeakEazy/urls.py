69fcdf88f5e304798a5aeb13967b62213578348f
SivaMahipathi <mahipathisiva@gmail.com>
2024-07-20 10:37:52 +0530

    third commit

--- SpeakEazy/urls.py
+++ SpeakEazy/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('speakeazy/',views.SpeakEazy,name='SpeakEazy'),
-    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('', TemplateView.as_view(template_name="home.html"), name="index"),
     path("accounts/", include("django.contrib.auth.urls")),
     path('voice-to-text/', views.voice_to_text, name='voice-to-text'), 
     path('text-to-voice/', views.text_to_voice, name='text-to-voice'),
